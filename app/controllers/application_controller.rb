ae5b6b1b0a16994f835e621c148dc7dd83aa3b1d
devmuturi <muturinjonge09@gmail.com>
2026-04-06 19:00:38 +0300

    Prepare the server for Turbo native app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Authenticate
 
   helper_method :turbo_frame_request?
+  helper_method :turbo_native_app?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
