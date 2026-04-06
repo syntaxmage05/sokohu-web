a9adaa40409f711de5bc6994582346d69d84e328
devmuturi <muturinjonge09@gmail.com>
2026-04-06 13:10:48 +0300

    Add logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,13 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    log_out
+
+    flash[:success] = t(".success")
+    redirect_to root_path, status: :see_other
+  end
+
 
   private
 
