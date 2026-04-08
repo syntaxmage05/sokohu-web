c8f6de3751babd031ea16cdb5ffaffd19f642af6
devmuturi <muturinjonge09@gmail.com>
2026-04-09 01:21:38 +0300

    Subtle fix error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,12 +55,12 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: ENV["https://sokohub-k2f4.onrender.com/"],
+    host: "https://sokohub-k2f4.onrender.com",
     protocol: "https"
   }
   config.action_mailer.delivery_method = :smtp
