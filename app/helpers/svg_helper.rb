13c93e2a904d90f0d998e6adaf7ec634a09d723a
devmuturi <muturinjonge09@gmail.com>
2026-04-10 21:40:33 +0300

    render svg on the show using helper

---
+++ app/helpers/svg_helper.rb
@@ -0,0 +1,13 @@
+module SvgHelper
+  def svg(svg_name, **attributes)
+    svg_markup = render file: "#{Rails.root}/app/views/shared/svg/#{svg_name}.svg"
+
+    xml = Nokogiri::XML(svg_markup)
+
+    attributes&.each do |key, value|
+      xml.root.set_attribute(key,value)
+    end
+
+    xml.root.to_xml.html_safe
+  end
+end
