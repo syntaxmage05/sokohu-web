0041c07d3fbff5ce3544123ccf09942f22dbec83
devmuturi <muturinjonge09@gmail.com>
2026-04-12 21:52:32 +0300

    Add description to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,7 @@ end
     cover_photo: cover_photo_blob,
     price: Faker::Commerce.price.floor,
     condition: Listing.conditions.values.sample,
+    description: Faker::Lorem.paragraph,
     tags: Faker::Commerce.send(:categories, 4),
     address_attributes: {
       line_1: Faker::Address.building_number,
