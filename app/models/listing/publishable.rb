6f4192dc9d512d94d9e3bd465ac6f3fd528a9895
devmuturi <muturinjonge09@gmail.com>
2026-04-17 04:33:45 +0300

    Fix error

--- app/models/listing/publishable.rb
+++ app/models/listing/publishable.rb
@@ -4,7 +4,7 @@ module Listing::Publishable
   extend ActiveSupport::Concern
 
   included do
-    before_commit :set_published_date, if: :published?
+    before_create :set_published_date, if: :published?
     before_save :set_published_date,
       if: -> { status_change_to_be_saved in [_, "published"] }
   end
