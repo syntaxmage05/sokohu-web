29464a75f93fd8b3595dcccd70e6757d2251f7ba
devmuturi <muturinjonge09@gmail.com>
2026-04-20 16:17:06 +0300

    Write the messages controller test

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require "test_helper"
 class ConversationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:jerry)
+    @conversation = conversations(:kramer_jerry_1)
+    log_in @user
+  end
+  test "shows dual pane index on desktop" do
+      get conversations_path
+      assert_response :ok
+      assert_select "#conversations"
+      assert_select "turbo-frame#conversation"
+    end
+  test "can view messages for a conversation" do
+    get conversation_path(@conversation)
+    assert_response :ok
+    assert_select "##{dom_id(@conversation, :messages)}"
+  end
 end
