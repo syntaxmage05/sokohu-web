01ff8bbec003abd06cd14241b020a04981ac07f6
devmuturi <muturinjonge09@gmail.com>
2026-04-20 16:12:38 +0300

    create a inbox-style messages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class ConversationsController < ApplicationController
+  skip_authorization only: :index
+  before_action :set_conversation, only: :show
   def index
+    @conversations = Conversation.current_organization.for_display
   end
 
   def show
+    @message = @conversation.messages.build
   end
+
+  private
+
+    def set_conversation
+      @conversation = Conversation.find(params[:id])
+    end
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,6 +2,19 @@
 
 class Conversation < ApplicationRecord
   include AccessPolicy
+
+  scope :current_organization, -> {
+    where(seller: Current.organization)
+      .or(where(buyer: Current.organization))
+  }
+
+  scope :for_display, -> {
+    joins(:messages)
+      .includes(:seller, :listing)
+      .order(created_at: :desc)
+      .distinct
+  }
+
   belongs_to :listing
   belongs_to :seller, class_name: "Organization"
   belongs_to :buyer, class_name: "Organization"
